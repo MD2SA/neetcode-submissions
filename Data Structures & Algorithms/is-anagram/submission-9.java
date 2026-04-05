946a83318b3236a049858c6a925a1d78ddd8a46b
MD2SA <147189320+MD2SA@users.noreply.github.com>
2026-04-05 10:46:05 +0100

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] letters = new int[26];
+        for(char c : s.toCharArray())
+            letters[c-'a']++;
+        for(char c : t.toCharArray())
+            letters[c-'a']--;
+        for(int letter : letters) 
+            if(letter != 0) return false;
+        return true;
+    }
+
+}
