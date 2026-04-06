ffa5f2b1bd9a54da3cd1de0a1b63bdce4b1b163f
MD2SA <147189320+MD2SA@users.noreply.github.com>
2026-04-06 12:17:02 +0100

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.java
@@ -0,0 +1,14 @@
+class Solution {
+    // Given an array of integers numbers that
+    // is sorted in non-decreasing order. 
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0, right = numbers.length-1;
+        while(left<right) {
+            int sum = numbers[left] + numbers[right];
+            if(sum < target) left++;
+            else if(sum > target) right--;
+            else return new int[]{left+1,right+1};
+        }
+        return new int[]{};
+    }
+}
