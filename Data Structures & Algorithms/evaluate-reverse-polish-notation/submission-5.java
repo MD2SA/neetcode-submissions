e3df3bed6f4c63dac662a696321d8ed87ce31f8c
MD2SA <147189320+MD2SA@users.noreply.github.com>
2026-04-08 10:11:49 +0100

    Add: evaluate-reverse-polish-notation - submission-5

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-5.java
@@ -0,0 +1,27 @@
+class Solution {
+  public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for(String token : tokens) {
+			if(isOp(token)){
+				int val2 = stack.pop();
+				int val1 = stack.pop();
+				int r = makeOp(val1,val2,token);
+				stack.push(r);
+			} else {
+				stack.push(Integer.valueOf(token));
+			}
+		}
+		return stack.pop();
+    }
+    
+    private boolean isOp(String s) {
+			return "*+/-".contains(s);
+	}
+	
+	private int makeOp(int v1, int v2, String op) {
+			if("+".equals(op)) return v1+v2;
+			else if("*".equals(op)) return v1*v2;
+			else if("-".equals(op)) return v1-v2;
+			return v1/v2;
+	}
+}
