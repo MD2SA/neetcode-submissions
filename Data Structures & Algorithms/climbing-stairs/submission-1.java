717606c3afb731fc9e17b8a5ce970461be76d390
MD2SA <147189320+MD2SA@users.noreply.github.com>
2026-04-20 00:35:22 +0100

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.java
@@ -0,0 +1,11 @@
+class Solution {
+
+    int[] memo = new int[45];
+
+    public int climbStairs(int n) {
+        if(n <= 2) return n;
+        if(memo[n-1] == 0) 
+            memo[n-1] = climbStairs(n-1)+climbStairs(n-2);
+        return memo[n-1];
+    }
+}
